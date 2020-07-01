469b92be810495cf6a5627d1a91bb996470e77c4
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-01 14:21:38 +0800

    1. 完成scala的Word Count函数

---
+++ quickstart-scala/src/main/scala/org/myorg/quickstart/course02/BatchJobWCScalaApp.scala
@@ -0,0 +1,24 @@
+package org.myorg.quickstart.course02
+
+import org.apache.flink.api.scala._
+
+object BatchJobWCScalaApp {
+
+  def main(args: Array[String]): Unit = {
+    val input = "file:///D:\\Users\\tyraelhuang\\tmp\\flink\\input"
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val text = env.readTextFile(input)
+
+    text.print()
+
+    // TODO
+    text.flatMap(_.toLowerCase.split("\t"))
+      .filter(_.nonEmpty)
+      .map((_,1))
+      .groupBy(0)
+      .sum(1)
+      .print()
+  }
+}
---
+++ quickstart/src/main/java/org/myorg/quickstart/course02/StreamingWCJavaApp.java
@@ -0,0 +1,8 @@
+package org.myorg.quickstart.course02;
+
+/**
+ * 使用Java API来开发Flink的批处理应用程序
+ */
+public class StreamingWCJavaApp {
+
+}
