4b17298c5e3b4cf63228719b676680fbcb0256bd
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-01 17:04:35 +0800

    1. Flink + Java实现流式计算wordcount 将key通过类进行指定

---
+++ quickstart/src/main/java/org/myorg/quickstart/course03/StreamingWCJavaApp03.java
@@ -0,0 +1,111 @@
+package org.myorg.quickstart.course03;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * 使用Java API来开发Flink的实时处理应用程序
+ * <p>
+ * wc统计的数据源自于socket
+ */
+public class StreamingWCJavaApp03 {
+
+    public static void main(String[] args) throws Exception {
+
+        // 获取参数
+        int port;
+        try {
+            ParameterTool tool = ParameterTool.fromArgs(args);
+            port = tool.getInt("port");
+        }catch (Exception e){
+            System.err.println("端口未设置，使用默认端口9999");
+            port = 9999;
+        }
+        // step1：获取执行环境
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // step2: 读取数据
+        DataStreamSource<String> text = env.socketTextStream("localhost", port);
+
+        // step3： transform
+
+        text.flatMap(new FlatMapFunction<String, WordCcount>() {
+            @Override
+            public void flatMap(String value, Collector<WordCcount> collector) throws Exception {
+                String[] tokens = value.toLowerCase().split(",");
+
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        collector.collect(new WordCcount(token,1));
+                    }
+                }
+            }
+        }).keyBy("word")
+                .timeWindow(Time.seconds(5))
+                .sum("count")
+                .print()
+                .setParallelism(1);
+
+//        text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+//            @Override
+//            public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+//                String[] tokens = value.toLowerCase().split(",");
+//
+//                for (String token : tokens) {
+//                    if (token.length() > 0) {
+//                        collector.collect(new Tuple2<String, Integer>(token, 1));
+//                    }
+//                }
+//
+//            }
+//        }).keyBy(0).timeWindow(Time.seconds(5)).sum(1).print().setParallelism(1);
+
+
+
+        env.execute("StreamingWCJavaApp03");
+
+    }
+
+    public static class WordCcount {
+        private String word;
+        private int count;
+
+        public WordCcount() {
+        }
+
+        public WordCcount(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "WordCcount{" +
+                    "word='" + word + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
+
+}
