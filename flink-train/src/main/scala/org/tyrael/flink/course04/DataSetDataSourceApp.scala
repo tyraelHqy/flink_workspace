603202923c57baf5d541eaa6960aa647ef13b355
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-07 11:03:02 +0800

    1. 从文件或者文件夹创建DataSet之 Scala 实现

--- flink-train/src/main/scala/org/tyrael/flink/course04/DataSetDataSourceApp.scala
+++ flink-train/src/main/scala/org/tyrael/flink/course04/DataSetDataSourceApp.scala
@@ -2,11 +2,15 @@ package org.tyrael.flink.course04
 
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.api.scala._
+
 object DataSetDataSourceApp {
 
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
-    fromCollection(env)
+
+    // 从集合的方式读取DataSet
+    // fromCollection(env)
+    fromTextFile(env)
   }
 
   def fromCollection(env: ExecutionEnvironment): Unit = {
@@ -14,5 +18,16 @@ object DataSetDataSourceApp {
     val data = 1 to 10
     env.fromCollection(data).print()
 
+  }
+
+  def fromTextFile(env: ExecutionEnvironment): Unit = {
+
+    // 读取单个文件
+     val filePath = "file:///D:\\Users\\tyraelhuang\\IdeaProjects\\flink-workspace\\test-data\\input.txt"
+     env.readTextFile(filePath).print()
+
+    // 读取文件夹
+    // val filePath = "file:///D:\\Users\\tyraelhuang\\IdeaProjects\\flink-workspace\\test-data\\inputs"
+    // env.readTextFile(filePath).print()
   }
 }
