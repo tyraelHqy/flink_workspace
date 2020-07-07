f204a6f8f85716aca2a61528ed59211969e65272
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-07 16:33:28 +0800

    1. Transformation函数mapPartition之Scala实现

---
+++ flink-train/src/main/scala/org/tyrael/flink/course04/DBUtils.scala
@@ -0,0 +1,14 @@
+package org.tyrael.flink.course04
+
+import scala.util.Random
+
+object DBUtils {
+
+  def getConnection() = {
+    new Random().nextInt(10) + ""
+  }
+
+  def returnConnection(connection: String): Unit = {
+
+  }
+}
--- flink-train/src/main/scala/org/tyrael/flink/course04/DataSetTransformationApp.scala
+++ flink-train/src/main/scala/org/tyrael/flink/course04/DataSetTransformationApp.scala
@@ -3,13 +3,17 @@ package org.tyrael.flink.course04
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.api.scala._
 
+import scala.collection.mutable.ListBuffer
+
 object DataSetTransformationApp {
 
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
     //    mapFunction(env)
-    filterFunction(env)
+    //    filterFunction(env)
+    mapPartitionFunction(env)
   }
+
 
   def mapFunction(env: ExecutionEnvironment): Unit = {
     val data = env.fromCollection(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
@@ -25,5 +29,23 @@ object DataSetTransformationApp {
       .map(_ + 1)
       .filter(_ > 5)
       .print()
+  }
+
+  // DataSource 100个元素,把结果存储到数据库中
+  def mapPartitionFunction(env: ExecutionEnvironment): Unit = {
+    val students = new ListBuffer[String]
+    for (i <- 1 to 100) {
+      students.append("student: " + i)
+    }
+
+    val data = env.fromCollection(students).setParallelism(4)
+
+    data.mapPartition(x => {
+      val connection = DBUtils.getConnection()
+      println(connection + "...")
+      DBUtils.returnConnection(connection)
+
+      x
+    }).print()
   }
 }
