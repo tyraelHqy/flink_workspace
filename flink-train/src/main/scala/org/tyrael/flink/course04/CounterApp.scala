bf578feac25cbb0139275db36f7baeeb9b64c80c
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-07 20:59:31 +0800

    1. 基于Flink编程的计时器之Scala实现

--- flink-train/src/main/scala/org/tyrael/flink/course04/CounterApp.scala
+++ flink-train/src/main/scala/org/tyrael/flink/course04/CounterApp.scala
@@ -1,8 +1,12 @@
 package org.tyrael.flink.course04
 
+import com.sun.corba.se.spi.ior.Writeable
+import org.apache.flink.api.common.accumulators.LongCounter
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.java.ExecutionEnvironment
 import org.apache.flink.api.java._
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.core.fs.FileSystem.WriteMode
 
 object CounterApp {
 
@@ -12,16 +16,42 @@ object CounterApp {
 
     val data = env.fromElements("hadoop", "spark", "flink", "pyspark", "storem")
 
-    data.map(new RichMapFunction[String, Long]() {
-      var counter = 0l;
+    //    data.map(new RichMapFunction[String, Long]() {
+    //      var counter = 0l;
+    //
+    //      override def map(value: String): Long = {
+    //        counter = counter + 1
+    //        println("counter:"+counter)
+    //        counter
+    //      }
+    //    }).setParallelism(3).print()
 
-      override def map(value: String): Long = {
-        counter = counter + 1
-        println("counter:"+counter)
-        counter
+    //    data.print()
+
+    val info = data.map(new RichMapFunction[String, String]() {
+
+      // step1: 定义计数器
+      val counter = new LongCounter()
+
+      override def open(parameters: Configuration): Unit = {
+
+        // step2：注册计数器
+        getRuntimeContext.addAccumulator("ele-counts-scala", counter)
+
       }
-    }).setParallelism(3).print()
 
-    data.print()
+      override def map(input: String): String = {
+        counter.add(1)
+        input
+      }
+    })
+    val filename = "file:///D:\\Users\\tyraelhuang\\IdeaProjects\\flink-workspace\\test-data\\sink-scala-count-out"
+    info.writeAsText(filename, WriteMode.OVERWRITE).setParallelism(5)
+    val jobResult = env.execute("CounterApp")
+
+    // 获取计数器
+    val num = jobResult.getAccumulatorResult[Long]("ele-counts-scala")
+    println("num:" + num)
+
   }
 }
