4e24d751881384841143f264c886a9f630f931fd
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-07 16:42:56 +0800

    1. Transformation函数mapPartition之 Java 实现

--- flink-train/src/main/java/org/tryael/flink/course04/JavaDataSetTransformationApp.java
+++ flink-train/src/main/java/org/tryael/flink/course04/JavaDataSetTransformationApp.java
@@ -2,10 +2,14 @@ package org.tryael.flink.course04;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.MapPartitionFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.util.Collector;
+import org.tyrael.flink.course04.DBUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,7 +17,8 @@ public class JavaDataSetTransformationApp {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 //        mapFunction(env);
-        filterFunction(env);
+//        filterFunction(env);
+        mapPartitionFunction(env);
     }
 
     public static void mapFunction(ExecutionEnvironment env) throws Exception {
@@ -42,6 +47,33 @@ public class JavaDataSetTransformationApp {
             @Override
             public boolean filter(Integer input) throws Exception {
                 return input > 5;
+            }
+        }).print();
+    }
+
+    public static void mapPartitionFunction(ExecutionEnvironment env) throws Exception {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            list.add("student: " + i);
+        }
+        DataSource<String> data = env.fromCollection(list).setParallelism(6);
+
+//        data.map(new MapFunction<String, String>() {
+//            @Override
+//            public String map(String value) throws Exception {
+//                String connection = DBUtils.getConnection();
+//                System.out.println("Connection: " + connection);
+//                DBUtils.returnConnection(connection);
+//                return value;
+//            }
+//        }).print();
+
+        data.mapPartition(new MapPartitionFunction<String, String>() {
+            @Override
+            public void mapPartition(Iterable<String> values, Collector<String> out) throws Exception {
+                String connection = DBUtils.getConnection();
+                System.out.println("Connection: " + connection);
+                DBUtils.returnConnection(connection);
             }
         }).print();
     }
