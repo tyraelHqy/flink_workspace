3fb7e894e61b59eba422bd529b7e5e851f5eea28
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-07 11:11:41 +0800

    1. 从文件或者文件夹创建DataSet之 Java 实现

--- flink-train/src/main/java/org/tryael/flink/course04/JavaDataSetDataSourceApp.java
+++ flink-train/src/main/java/org/tryael/flink/course04/JavaDataSetDataSourceApp.java
@@ -1,6 +1,7 @@
 package org.tryael.flink.course04;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,11 +10,28 @@ import java.util.List;
 public class JavaDataSetDataSourceApp {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        fromCollection(env);
+
+        fromTextFile(env);
+//        fromCollection(env);
     }
 
     public static void fromCollection(ExecutionEnvironment env) throws Exception {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         env.fromCollection(list).print();
     }
+
+    public static void fromTextFile(ExecutionEnvironment env) throws Exception {
+
+        String filePath = "file:///D:\\Users\\tyraelhuang\\IdeaProjects\\flink-workspace\\test-data\\input.txt";
+
+        // step2: read data
+        DataSource<String> text = env.readTextFile(filePath);
+        text.print();
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~分割线~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        filePath = "file:///D:\\Users\\tyraelhuang\\IdeaProjects\\flink-workspace\\test-data\\inputs";
+        env.readTextFile(filePath).print();
+    }
+
 }
