aa056908d92a7ce68e41510fce0f23fbff10c170
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-08 15:06:41 +0800

    1. Transformation函数split和select之Java实现

--- flink-train/src/main/java/org/tryael/flink/course05/JavaDataStreamTransformationApp.java
+++ flink-train/src/main/java/org/tryael/flink/course05/JavaDataStreamTransformationApp.java
@@ -2,15 +2,21 @@ package org.tryael.flink.course05;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class JavaDataStreamTransformationApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 //        filterFunction(env);
-        unionFunction(env);
+//        unionFunction(env);
+        splitSelectFunction(env);
         env.execute("JavaDataStreamTransformationApp");
     }
 
@@ -26,5 +32,22 @@ public class JavaDataStreamTransformationApp {
         DataStreamSource<Long> data1 = env.addSource(new JavaCustomNonParallelSourceFunction());
         DataStreamSource<Long> data2 = env.addSource(new JavaCustomNonParallelSourceFunction());
         data1.union(data2).print();
+    }
+
+    private static void splitSelectFunction(StreamExecutionEnvironment env) {
+        DataStreamSource<Long> data = env.addSource(new JavaCustomNonParallelSourceFunction());
+        SplitStream<Long> splits = data.split(new OutputSelector<Long>() {
+            @Override
+            public Iterable<String> select(Long value) {
+                List<String> output = new ArrayList<>();
+                if(value % 2 == 0){
+                    output.add("even");
+                }else {
+                    output.add("odd");
+                }
+                return output;
+            }
+        });
+        splits.select("odd","even").print();
     }
 }
