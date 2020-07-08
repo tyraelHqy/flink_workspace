d0810b393d5d2169896e9efe307daa6428081017
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-07-08 11:27:33 +0800

    1. 自定义数据源方式SourceFunction之Scala实现

--- flink-train/src/main/scala/org/tyrael/flink/course05/DataStreamSourceApp.scala
+++ flink-train/src/main/scala/org/tyrael/flink/course05/DataStreamSourceApp.scala
@@ -1,20 +1,27 @@
 package org.tyrael.flink.course05
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
 
 object DataStreamSourceApp {
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    socketFunction(env)
+    //    socketFunction(env)
 
+    nonParallelSourceFunction(env)
     env.execute("DataStreamSourceApp")
   }
 
   def socketFunction(env: StreamExecutionEnvironment): Unit = {
-    val data = env.socketTextStream("localhost",9999)
-
+    val data = env.socketTextStream("localhost", 9999)
     data.print().setParallelism(1)
+  }
+
+  def nonParallelSourceFunction(env: StreamExecutionEnvironment): Unit = {
+    val data = env.addSource(new CustomNonParallelSourceFunction).setParallelism(1)
+    data.print().setParallelism(1)
+
   }
 }
